8a009366135231d79eb4e467a68e68c08dbfd529
Ismael M <jmorillolabour.cf@iesesteveterradas.cat>
2023-02-16 16:26:32 +0100

    Base Commit

---
+++ app/src/main/java/com/example/cornapp/view/profile/User.java
@@ -0,0 +1,14 @@
+package com.example.cornapp.view.profile;
+
+public class User {
+    private String name, surname, email;
+    private int phone;
+
+    public User(String _name, String _surname, String _email, int _phone){
+        this.name = _name;
+        this.surname = _surname;
+        this.email = _email;
+        this.phone = _phone;
+    }
+
+}
