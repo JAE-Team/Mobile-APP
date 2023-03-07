aee223ecc49f6fb901d540feb770460c4a1c1b62
Ismael M <jmorillolabour.cf@iesesteveterradas.cat>
2023-03-07 20:38:00 +0100

    SPEC 33 i SPEC 32 comenzada

--- app/src/main/java/com/cornApp/view/perfil/PerfilFragment.java
+++ app/src/main/java/com/cornApp/view/perfil/PerfilFragment.java
@@ -1,18 +1,16 @@
 package com.cornApp.view.perfil;
 
-import static android.app.Activity.RESULT_OK;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
+import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,23 +18,28 @@ import android.view.ViewGroup;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import com.cornApp.LoginActivity;
 import com.cornApp.R;
 import com.cornApp.databinding.PerfilFragmentBinding;
+import com.cornApp.utils.FileUtil;
 import com.cornApp.utils.Utils;
 import com.cornApp.utils.UtilsHTTP;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 public class PerfilFragment extends Fragment {
     private PerfilFragmentBinding binding;
-    private ActivityResultLauncher<Intent> launcher;
+    ActivityResultLauncher<Intent> launcher;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = PerfilFragmentBinding.inflate(inflater, container, false);
@@ -51,13 +54,22 @@ public class PerfilFragment extends Fragment {
                 if (clipData != null && clipData.getItemCount() >= 2) {
                     Uri uri1 = clipData.getItemAt(0).getUri();
                     Uri uri2 = clipData.getItemAt(1).getUri();
-                    // Procesar las imágenes seleccionadas
+
+                    try {
+                        File anvers = FileUtil.from(getActivity(),uri1);
+                        File revers = FileUtil.from(getActivity(),uri2);
+                        uploadFile(anvers,revers);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+
                 }
             }
         });
 
         binding.dni.setOnClickListener(v -> {
             startGallery();
+
         });
 
         binding.logout.setOnClickListener(v -> {
@@ -84,6 +96,64 @@ public class PerfilFragment extends Fragment {
         return binding.getRoot();
     }
 
+    private void uploadFile (File anversDNI, File reversDNI) {
+
+        try {
+            // Read image from File and convert to Base64
+            byte[] anvers = new byte[0];
+            byte[] revers = new byte[0];
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                anvers = Files.readAllBytes(Paths.get(anversDNI.getPath()));
+                revers = Files.readAllBytes(Paths.get(reversDNI.getPath()));
+            }
+
+            String anversBase64 = null;
+            String reversBase64 = null;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                anversBase64 = Base64.getEncoder().encodeToString(anvers);
+                reversBase64 = Base64.getEncoder().encodeToString(revers);
+            }
+
+            JSONObject obj = new JSONObject("{}");
+            obj.put("type", "uploadFile");
+            obj.put("anvers", anversBase64);
+            obj.put("revers", reversBase64);
+
+            UtilsHTTP.sendPOST(Utils.apiUrl + "/api/send_id", obj.toString(), (response) -> {
+                try {
+                    JSONObject objResponse = new JSONObject(response);
+                    if (objResponse.getString("status").equals("OK")) {
+                        popupMessage("Imatges pujades a la BBDD");
+                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+        } catch (IOException e) { e.printStackTrace(); } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static File getFileFromUri(Context context, Uri uri) {
+        File file = null;
+        String filePath = null;
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            filePath = cursor.getString(column_index);
+            cursor.close();
+        }
+
+        if (filePath != null) {
+            file = new File(filePath);
+        }
+
+        return file;
+    }
+
     public void logout(String response) throws JSONException {
         JSONObject objResponse = new JSONObject(response);
 
@@ -100,10 +170,10 @@ public class PerfilFragment extends Fragment {
     public void startGallery(){
         //Create Intent
         Intent intent = new Intent();
-        intent.setType("image/jpg");
+        intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        startActivity(Intent.createChooser(intent,"Escull dos fotos: "));
+        launcher.launch(Intent.createChooser(intent,"Escull dos fotos: "));;
     }
 
     public void getProfileInfo(){
